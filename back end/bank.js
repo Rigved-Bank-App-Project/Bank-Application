4c1d91cde20f1c0f18d95d06c1f15fd4d2ffa8fe
RohitGarate <79219233+RohitGarate@users.noreply.github.com>
2022-04-26 15:05:49 +0530

    bank.js

--- back end/bank.js
+++ back end/bank.js
@@ -1,1 +1,20 @@
-//
+//import all functions
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let cors = require('cors');
+let mongoClient = require('mongodb').MongoClient;
+
+let PORT = 3001;
+let dbURL = "mongodb://localhost:27017";
+
+//start the server
+app.listen(PORT, () => {
+    console.log(`Server is running in ${PORT}`);
+});
+
+//apply the middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//create the the services for contact app

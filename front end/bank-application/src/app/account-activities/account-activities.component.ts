4cfeea27348ba76448e190d3b681e621820f8173
RohitGarate <79219233+RohitGarate@users.noreply.github.com>
2022-04-27 17:56:25 +0530

    Rohit pushed some code

---
+++ front end/bank-application/src/app/account-activities/account-activities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-activities',
+  templateUrl: './account-activities.component.html',
+  styleUrls: ['./account-activities.component.css']
+})
+export class AccountActivitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

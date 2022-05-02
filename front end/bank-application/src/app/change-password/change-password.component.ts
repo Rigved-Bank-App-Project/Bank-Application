f4ce5e9a018c1beea9676326a933b9e9e0c60870
abhishekshetty <abhishetty563@gmail.com>
2022-05-02 15:22:49 +0530

    doneee

--- front end/bank-application/src/app/change-password/change-password.component.ts
+++ front end/bank-application/src/app/change-password/change-password.component.ts
@@ -30,3 +30,42 @@ export class ChangePasswordComponent implements OnInit {
     })
   }
 }
+
+
+
+
+
+//   modifiedCount:undefined| any=undefined
+//   constructor(private _actived_rout:ActivatedRoute, private _customer_service:CustomerService,private _transaction_service:TransactionService, private _rout:Router) { }
+
+//   ngOnInit(): void {
+    
+//   }
+
+//   password=new FormControl('',Validators.required)
+//   again_pass=new FormControl('',Validators.required)
+//   handleUpdate(){
+//     if(this.password.value==this.again_pass.value){
+//       this._actived_rout.parent?.parent?.params.subscribe({
+//         next:(params:Params)=>{
+//           this._customer_service.updateLoginPass(params['cust_id'],this.password.value,undefined).subscribe({
+//             next:(data)=>{console.log(data)
+//               //this.modifiedCount=data.modifiedCount
+//             }
+//           });
+//           this._transaction_service.updatePasslogin(params['cust_id'],params['pass'],this.password.value,undefined).subscribe({
+//             next:(data)=>console.log(data)
+//           })
+          
+          
+//           this._rout.navigate(['/home'])
+//         }
+//       })
+
+//     } else{
+      
+//       alert('password is not match')
+//     }
+//   }
+
+// }
